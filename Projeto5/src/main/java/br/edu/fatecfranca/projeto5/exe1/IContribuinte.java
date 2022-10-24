85386fb152dd0bb5afe2b96aaf83bc3a352fb709
Leonardo Mendonça Ferreira <54585188+Leomendferre@users.noreply.github.com>
2022-10-24 12:36:20 -0300

    Add files via upload

---
+++ Projeto5/src/main/java/br/edu/fatecfranca/projeto5/exe1/IContribuinte.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.edu.fatecfranca.projeto5.exe1;
+
+/**
+ *
+ * @author danielfacciolo
+ */
+public interface IContribuinte {
+    
+    public void pagaIr();
+    
+    public void obterCpf();
+    
+    
+    
+}
---
+++ Projeto5/src/main/java/br/edu/fatecfranca/projeto5/exe1/IProfessor.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.edu.fatecfranca.projeto5.exe1;
+
+/**
+ *
+ * @author danielfacciolo
+ */
+public interface IProfessor extends IEmpregado{
+    
+    public void ensina();
+    
+    // ele vai herdar o método trabalha()
+    
+}
